75ea2808c567315e210e79a638bfd549567e553e
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2017-11-26 21:21:48 +0000

    Added in code example B

---
+++ nodelessons/B_Types.js
@@ -0,0 +1,30 @@
+ // See https://thefriendlytester.co.uk/programming/lessons/typesandobjects for a more detailed explanation of types
+var myInt         = 33;
+var myFloat       = 33.33;
+var myBoolean     = true;
+var myString      = "Mark loves cake";
+var myIntArray    = [1, 2, 3];
+var myStringArray = ["Mark", "Node", "Testing"];
+
+// Because myInt is an integer, Node allows me to do math with it
+// If you run this, you should get 330 in the console
+console.log(myInt * 10);
+
+// Same applies to floats. This should return you 333.3
+console.log(myFloat * 10);
+
+// This will print true
+console.log(myBoolean);
+
+// This will print "Mark loves cake"
+console.log(myString);
+
+// This will print "["Mark", "Node", "Testing"]" as it's an array of strings
+console.log(myStringArray);
+
+// But we can also print a single string from the array by providing an index value
+// If we wanted the string "Node" we can call myStringArray[1]. Notice how the second
+// item is called with the number one. That's because arrays are zero indexed meaning
+// 0 willl return the first array item.
+console.log(myStringArray[1]);
+console.log(myIntArray[0]);
