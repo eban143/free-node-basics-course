2a95f629d157e66741cfecb7aedb06e4527d307b
Mark Winteringham <mark@mwtestconsultancy.co.uk>
2017-11-26 21:30:36 +0000

    Added in more variable examples to example B

--- nodelessons/B_Types.js
+++ nodelessons/B_Types.js
@@ -5,6 +5,13 @@ var myBoolean     = true;
 var myString      = "Mark loves cake";
 var myIntArray    = [1, 2, 3];
 var myStringArray = ["Mark", "Node", "Testing"];
+var myFunction    = function(){
+                        return "Hello";
+                    }
+var myObject      = {
+    "myObjectsInt" : 33,
+    "myObjectsString" : "I live in an object"
+}
 
 // Because myInt is an integer, Node allows me to do math with it
 // If you run this, you should get 330 in the console
@@ -28,3 +35,10 @@ console.log(myStringArray);
 // 0 willl return the first array item.
 console.log(myStringArray[1]);
 console.log(myIntArray[0]);
+
+// This will print the output of our function which will be "Hello"
+console.log(myFunction());
+
+// This will print out the string "I live in an object" as it's a variable that lives
+// within an object
+console.log(myObject.myObjectsString);
